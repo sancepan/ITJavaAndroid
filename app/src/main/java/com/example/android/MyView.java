37fdba7ce03f2e53c60be685d4116a95779e1755
sancepan <sancepance1@mail.ru>
2019-11-06 14:15:26 +0300

    1.8.9 Solar system

--- app/src/main/java/com/example/android/MyView.java
+++ app/src/main/java/com/example/android/MyView.java
@@ -1,129 +1,96 @@
 package com.example.android;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.Shader;
+import android.os.CountDownTimer;
 import android.view.View;
 
+@SuppressLint("DrawAllocation")
 public class MyView extends View {
-    double xe, ye, w;
-    double x, y, h = 0.1;
-    int k = 100;
-    //double xmin = -2 * Math.PI, xmax = 2 * Math.PI;
-    double xmin = -4, xmax = 4;
+    int N = 5;
+    int[] x = new int[N];
+    int[] y = new int[N];
+    int[] vx = new int[N];
+    int[] vy = new int[N];
+    int[] L = new int[N];
+    int[] Red = new int[N];
+    int[] Green = new int[N];
+    int[] Blue = new int[N];
+    int[] R = new int[N];
+    int z = -1;
+    double a = 0, ha = Math.PI / 180;
 
-    public MyView(Context context) {
+    void fillArrayRandom(int[] a, int min, int max) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * (max - min + 1)) + min;
+        }
+    }
+
+    void makeBalls() {
+        fillArrayRandom(x, 50, 250);
+        fillArrayRandom(y, 50, 250);
+        fillArrayRandom(vx, -50, 100);
+        fillArrayRandom(vy, -50, 100);
+        fillArrayRandom(L, 3, 10);
+        fillArrayRandom(Red, 50, 255);
+        fillArrayRandom(Green, 50, 255);
+        fillArrayRandom(Blue, 50, 255);
+        fillArrayRandom(R, 20, 40);
+    }
+
+    void moveBalls() {
+        for (int i = 0; i < N; i++) {
+            if (i % 2 == 0) {
+                x[i] = this.getWidth() / 2 + (int) (L[i] * vx[i] * Math.cos(a));
+                y[i] = this.getHeight() / 2 + (int) (z * L[i] * vy[i] * Math.sin(a));
+            } else {
+                x[i] = this.getWidth() / 2 + (int) (L[i] * vx[i] * Math.cos(a));
+                y[i] = this.getHeight() / 2 + (int) (L[i] * vy[i] * Math.sin(a));
+            }
+        }
+        a = a + ha;
+    }
+
+    MyView(Context context) {
         super(context);
+        makeBalls();
+        MyTimer timer = new MyTimer();
+        timer.start();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
-        int x0 = this.getWidth() / 2;
-        int y0 = this.getHeight() / 2;
-
         Paint paint = new Paint();
-        paint.setStrokeWidth(5);
-        paint.setColor(Color.BLACK);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setTextSize(30.0f);
-
-        canvas.drawLine(x0, 0, x0, this.getHeight(), paint);
-        canvas.drawText("Y", this.getWidth() / 2 + 10, 35, paint);
-        canvas.drawLine(0, y0, this.getWidth(), y0, paint);
-        canvas.drawText("X", this.getWidth() - 50, this.getHeight() / 2 + 50, paint);
-
-        Path path = new Path();
-        boolean first = true;
-        paint.setColor(Color.BLUE);
-        for (x = xmin; x < xmax; x += h) {
-
-            y=Math.abs(x);
-            //y = Math.pow(x,4);
-            // y=Math.tan(x);
-            // y = 3 * Math.sin(x);
-            xe = x0 + k * x;
-            ye = y0 - k * y;
-            if (first) {
-                path.moveTo((float) xe, (float) ye);
-                first = false;
-            } else {
-                path.lineTo((float) xe, (float) ye);
-            }
-            canvas.drawPath(path, paint);
-        }
-        Path path2 = new Path();
-        first = true;
-        paint.setColor(Color.RED);
-        for (x = xmin; x < xmax; x += h) {
-            y=-x*x+3;
-            // y=Math.tan(x);
-            // y = 3 * Math.sin(x);
-            xe = x0 + k * x;
-            ye = y0 - k * y;
-            if (first) {
-                path2.moveTo((float) xe, (float) ye);
-                first = false;
-            }
-            else
-            {
-                path2.lineTo((float) xe, (float) ye);
-            }
-            canvas.drawPath(path2, paint);
-        }
-        Path path3 = new Path();
-        first = true;
-        paint.setColor(Color.GREEN);
-        for (x = -1.5; x < 1.6; x += h) {
-            y=Math.tan(x);
-            xe = x0 + k * x;
-            ye = y0 - k * y;
-            if (first) {
-                path3.moveTo((float) xe, (float) ye);
-                first = false;
-            }
-            else
-            {
-                path3.lineTo((float) xe, (float) ye);
-            }
-            canvas.drawPath(path3, paint);
-        }
-        Path path4 = new Path();
-        first = true;
         paint.setColor(Color.YELLOW);
-        for (x = -6; x < 6; x += h) {
-             y = 3 * Math.sin(x);
-            xe = x0 + k * x;
-            ye = y0 - k * y;
-            if (first) {
-                path4.moveTo((float) xe, (float) ye);
-                first = false;
-            }
-            else
-            {
-                path4.lineTo((float) xe, (float) ye);
-            }
-            canvas.drawPath(path4, paint);
+        canvas.drawCircle(this.getWidth() / 2, this.getHeight() / 2, 70, paint);
+        paint.setStyle(Paint.Style.FILL);
+        for (int i = 0; i < N; i++) {
+            paint.setColor(Color.argb(200, Red[i], Green[i], Blue[i]));
+            canvas.drawCircle(x[i], y[i], R[i], paint);
+            paint.setTextSize(30.0f);
+            paint.setColor(Color.BLACK);
+            canvas.drawText("P " + i + " (" + x[i] + ", " + y[i] + ")", x[i] + 10, y[i] - 15, paint);
         }
-        Path path5 = new Path();
-        first = true;
-        paint.setColor(Color.GRAY);
-        for (x = -3.1; x < 1.6; x += h) {
-            y = 1/Math.tan(x);
-            xe = x0 + 155 + k * x;
-            ye = y0 - k * y;
-            if (first) {
-                path5.moveTo((float) xe, (float) ye);
-                first = false;
-            }
-            else
-            {
-                path5.lineTo((float) xe, (float) ye);
-            }
-            canvas.drawPath(path5, paint);
+    }
+    void nextFrame() {
+        moveBalls();
+        invalidate();
+    }
+    class MyTimer extends CountDownTimer {
+        MyTimer() {
+            super(1000000, 1);
+        }
+        @Override
+        public void onTick(long millisUntilFinished) {
+            // TODO Auto-generated method stub
+            nextFrame();
+        }
+        @Override
+        public void onFinish() {
+            // TODO Auto-generated method stub
         }
     }
 }
