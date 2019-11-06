5a31da52dc3670655a673ac34507d50f5a73ee69
sancepan <sancepance1@mail.ru>
2019-11-06 13:15:37 +0300

    1.8.7 Simple animation

--- app/src/main/java/com/example/android/MyView.java
+++ app/src/main/java/com/example/android/MyView.java
@@ -5,33 +5,37 @@ import android.graphics.*;
 import android.view.View;
 
 public class MyView extends View {
+    int N = 10; // количество шариков
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
 
     public MyView(Context context) { // конструктор, необходимый для нормальной работы
         super(context);
+        for (int i = 0; i < N; i++){
+            x[i] = (float)(Math.random() * 500);
+            y[i] = (float)(Math.random() * 500);
+            vx[i] = (float)(Math.random() * 6 - 3);
+            vy[i] = (float)(Math.random() * 6 - 3);
+        }
     }
 
     @Override
 
     protected void onDraw(Canvas canvas) { // создаем метод
-        super.onDraw(canvas);
         Paint paint = new Paint();
-        paint.setStrokeWidth(5);
-        paint.setColor(Color.RED);
-        canvas.drawRect(190, 866, getHeight(), getHeight(), paint);
-        paint.setColor(Color.GREEN);
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        canvas.drawLine(5, 10, 15, 800, paint);
-        paint.setColor(Color.MAGENTA);
-        paint.setColor(Color.YELLOW);
-        canvas.drawCircle(10, 410, 350, paint);
-        canvas.drawLine(0, 500, this.getWidth(), 600, paint);
-        paint.setColor(Color.GREEN);
-        canvas.drawLine(6900, 10, 400, 900, paint);
-        paint.setColor(Color.YELLOW);
-        paint.setStrokeWidth(20);
-        canvas.drawLine(500, 10, 450, 800, paint);
-        paint.setColor(Color.CYAN);
-        canvas.drawCircle(600, 750, 150, paint);
+        super.onDraw(canvas);
+        // отрисовываем все шарики
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
+        }
+        // готовим массивы x и у для следущего кадра
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }
+        //запрашиваем перерисовку
+        invalidate();
     }
 }
