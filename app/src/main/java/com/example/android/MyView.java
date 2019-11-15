ed1e74cadcd93a5a8686982cfad0d5de78b3f67a
sancepan <sancepance1@mail.ru>
2019-11-15 16:58:33 +0300

    2.4 Equation (corrected)

--- app/src/main/java/com/example/android/MyView.java
+++ app/src/main/java/com/example/android/MyView.java
@@ -1,38 +1,82 @@
 package com.example.android;
 
 import android.content.Context;
-//import android.graphics.Canvas;
-import android.graphics.*;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.View;
 
 public class MyView extends View {
 
-    public MyView(Context context) { // конструктор, необходимый для нормальной работы
+    Paint paint = new Paint();
+
+    // Возвращает псевдослучайное число
+    // в полуинтервале [min, max)
+    float rand(float min , float max){
+        return (float)(Math.random() * (max - min + 1)) + min;
+    }
+
+    // Заполняет массив псевдослучайными числами в полуинтервале [min, max)
+    void fillRandom(float[] array , float min, float max){
+        for (int i = 0; i < array.length; i++){
+            array[i] = rand (min, max);
+        }
+    }
+
+    // Прибавляет  к элементам первого массива значения элеметов второго
+    // Массивы должны быть одинаковой длины!
+    void addValues(float[] array , float[] values){
+        for (int i = 0; i < array.length; i++){
+            array[i] += values[i];
+        }
+    }
+
+    void drawBalls(Canvas canvas){
+        for (int i = 0; i < 9; i++) {
+
+            canvas.drawCircle(x[i], y[i], 20, paint);
+
+        }
+    }
+
+    void Proverk4(int i, float[] x, float[] y, float[] vx, float[] vy){
+            if (x[i] <= 0 || x[i] >= this.getWidth()){
+                vx[i] = - vx[i];
+            }
+            if (y[i] <= 0 || y[i] >= this.getHeight()){
+                vy[i] = - vy[i];
+            }
+    }
+
+    int N = 10; // количество шариков
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
+
+    public MyView(Context context) {
         super(context);
+        fillRandom(x, 0, 500);
+        fillRandom(y, 0, 500);
+        fillRandom(vx, -3, 3);
+        fillRandom(vy, -3, 3);
     }
 
     @Override
-
-    protected void onDraw(Canvas canvas) { // создаем метод
+    protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
-        Paint paint = new Paint();
-        paint.setStrokeWidth(5);
-        paint.setColor(Color.RED);
-        int y = canvas.getHeight();
-        int x = 0;
-        while (y > 0) {
-            canvas.drawLine(0, y,
-                    x, canvas.getHeight(), paint);
-            y -= canvas.getHeight()/20;
-            x += this.getWidth()/20;
+        // отрисовываем все шарики
+        drawBalls(canvas);
+
+
+        // готовим массивы x и у для следущего кадра
+        for (int i = 0; i < N; i++) {
+            // Делаем проверку на границы
+            Proverk4(i, x, y, vx, vy);
+            addValues(x, vx);
+            addValues(y, vy);
         }
-        y = canvas.getHeight() - 70;
-        x = 50;
-        while (y > 0) {
-            canvas.drawLine(x, 0,
-                    this.getWidth(), y, paint);
-            y -= canvas.getHeight()/20;
-            x += this.getWidth()/20;
-        }
+
+        //запрашиваем перерисовку
+        invalidate();
     }
 }
